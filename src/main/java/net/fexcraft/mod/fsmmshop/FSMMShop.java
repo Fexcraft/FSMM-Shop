abd8c58ba5265d5028260cfa2766c2871679a4b8
Ferdinand Calo <fexcalo@gmail.com>
2024-01-30 19:01:29 +0100

    Update FSMMShop.java

--- src/main/java/net/fexcraft/mod/fsmmshop/FSMMShop.java
+++ src/main/java/net/fexcraft/mod/fsmmshop/FSMMShop.java
@@ -1,9 +1,14 @@
 package net.fexcraft.mod.fsmmshop;
 
+import net.fexcraft.app.json.JsonHandler;
+import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.mc.registry.FCLRegistry;
 import net.fexcraft.lib.mc.render.FCLBlockModel;
 import net.fexcraft.lib.mc.render.FCLBlockModelLoader;
+import net.fexcraft.mod.fsmm.util.Command;
+import net.fexcraft.mod.fsmm.util.FSMMSubCommand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -14,16 +19,23 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.io.File;
+
 /**
  *
  * @author Ferdinand Calo' (FEX___96)
  *
  */
-@Mod(modid = "fsmmshop", name = "FSMM Shop", version = "1.0.2", dependencies = "after:fsmm")
+@Mod(modid = "fsmmshop", name = "FSMM Shop", version = FSMMShop.VERSION, dependencies = "after:fsmm")
 public class FSMMShop {
 
     @Instance("fsmmshop")
     public static FSMMShop INSTANCE;
+    public static final String VERSION = "1.0.3";
+    //
+    public static File CFGFILE;
+    public static int MAXUSEBALANCE;
+    public static String MAXUSEBALMSG;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) throws InstantiationException, IllegalAccessException {
@@ -31,12 +43,25 @@ public class FSMMShop {
         if(event.getSide().isClient()){
             FCLBlockModelLoader.addBlockModel(new ResourceLocation("fsmmshop:models/block/shop"), ((Class<FCLBlockModel>)FCLRegistry.getModel("fsmmshop:models/block/shop")).newInstance());
         }
+        CFGFILE = new File(event.getSuggestedConfigurationFile().getParentFile(), "fsmm_shop.json");
+        if(!CFGFILE.exists()){
+            JsonMap map = new JsonMap();
+            map.add("max-use-balance", MAXUSEBALANCE = 0);
+            map.add("max-use-balance-msg", MAXUSEBALMSG = "&eYour balance is too high to use admin shops.");
+            JsonHandler.print(CFGFILE, map, JsonHandler.PrintOption.DEFAULT);
+        }
+        else{
+            JsonMap map = JsonHandler.parse(CFGFILE);
+            MAXUSEBALANCE = map.getInteger("max-use-balance", 0);
+            MAXUSEBALMSG = map.getString("max-use-balance-msg", "&eYour balance is too high to use admin shops.");
+        }
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event){
         if(event.getSide().isClient()) regtileren();
         NetworkRegistry.INSTANCE.registerGuiHandler(INSTANCE, new GuiHandler());
+        FSMMSubCommand.register("shop", new ShopSubCmd());
     }
 
     @SideOnly(Side.CLIENT)
