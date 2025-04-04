65f8520eca8779842a6a002d8b243ec476ffe484
Ferdinand Calo <fexcalo@gmail.com>
2025-04-04 13:18:35 +0200

    Update GuiHandler.java

--- forge12/src/main/java/net/fexcraft/mod/fsmmshop/GuiHandler.java
+++ forge12/src/main/java/net/fexcraft/mod/fsmmshop/GuiHandler.java
@@ -1,8 +1,6 @@
 package net.fexcraft.mod.fsmmshop;
 
-import net.fexcraft.mod.fsmmshop.ui.ShopContainer;
-import net.fexcraft.mod.fsmmshop.ui.ShopEditUI;
-import net.fexcraft.mod.fsmmshop.ui.ShopViewUI;
+import net.fexcraft.mod.uni.ui.UIUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
@@ -13,14 +11,11 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 public class GuiHandler implements IGuiHandler {
 
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-        if(ID < 2) return new ShopContainer(ID, player, world, x, y, z);
-        return null;
+        return UIUtils.getServer("fsmmshop", ID, player, x, y, z);
     }
 
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-        if(ID == 0) return new ShopEditUI(player, world, x, y, z);
-        if(ID == 1) return new ShopViewUI(player, world, x, y, z);
-        return null;
+        return UIUtils.getClient("fsmmshop", ID, player, x, y, z);
     }
 
 }
