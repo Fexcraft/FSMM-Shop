887f5bd9b0a25356c069bb423027344e6f03a77e
Ferdinand Calo <fexcalo@gmail.com>
2025-04-08 22:50:22 +0200

    Update ShopEditorCon.java

--- common/java/net/fexcraft/mod/fsmmshop/ui/ShopEditorCon.java
+++ common/java/net/fexcraft/mod/fsmmshop/ui/ShopEditorCon.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.fsmmshop.ui;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fsmmshop.FSMMShop;
+import net.fexcraft.mod.fsmmshop.Shop;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 
@@ -11,9 +12,12 @@ import net.fexcraft.mod.uni.ui.ContainerInterface;
  */
 public class ShopEditorCon extends ContainerInterface {
 
+	protected Shop shop;
+
 	public ShopEditorCon(JsonMap map, UniEntity ply, V3I pos){
 		super(map, ply, pos);
-		inventory = FSMMShop.getInvAt(ply.entity.getWorld().local(), pos);
+		shop = FSMMShop.getShopAt(ply.entity.getWorld().local(), pos);
+		inventory = shop.inventory;
 	}
 
 }
