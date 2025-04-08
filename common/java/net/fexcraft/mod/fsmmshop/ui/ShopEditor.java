ec7d086bd67458f5f7219d20afe8a121983a0120
Ferdinand Calo <fexcalo@gmail.com>
2025-04-08 22:19:03 +0200

    Create ShopEditor.java

---
+++ common/java/net/fexcraft/mod/fsmmshop/ui/ShopEditor.java
@@ -0,0 +1,16 @@
+package net.fexcraft.mod.fsmmshop.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.ui.UserInterface;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ShopEditor extends UserInterface {
+
+	public ShopEditor(JsonMap map, ContainerInterface container) throws Exception{
+		super(map, container);
+	}
+
+}
