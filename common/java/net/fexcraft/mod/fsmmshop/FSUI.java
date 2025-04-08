bd286dbe7a79b0a7f8aa0417b836fbeed34db858
Ferdinand Calo <fexcalo@gmail.com>
2025-04-08 22:19:01 +0200

    Update FSUI.java

--- common/java/net/fexcraft/mod/fsmmshop/FSUI.java
+++ common/java/net/fexcraft/mod/fsmmshop/FSUI.java
@@ -1,5 +1,7 @@
 package net.fexcraft.mod.fsmmshop;
 
+import net.fexcraft.mod.fsmmshop.ui.ShopEditor;
+import net.fexcraft.mod.fsmmshop.ui.ShopEditorCon;
 import net.fexcraft.mod.uni.UniReg;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UIKey;
@@ -14,8 +16,8 @@ public class FSUI {
 	public static UIKey SHOP_VIEWER = new UIKey(1, "fsmmshop:viewer");
 
 	public static void register(){
-		UniReg.registerUI(SHOP_EDITOR, UserInterface.class);
-		UniReg.registerMenu(SHOP_EDITOR, "fsmmshop:uis/shop_editor", ContainerInterface.class);
+		UniReg.registerUI(SHOP_EDITOR, ShopEditor.class);
+		UniReg.registerMenu(SHOP_EDITOR, "fsmmshop:uis/shop_editor", ShopEditorCon.class);
 		UniReg.registerUI(SHOP_VIEWER, UserInterface.class);
 		UniReg.registerMenu(SHOP_VIEWER, "fsmmshop:uis/shop_viewer", ContainerInterface.class);
 	}
