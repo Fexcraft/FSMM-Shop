74c5d542c0a42697b4814ab9f36c5ab7d32d6af0
Ferdinand Calo <fexcalo@gmail.com>
2025-04-09 23:25:04 +0200

    Update ShopEditor.java

--- common/java/net/fexcraft/mod/fsmmshop/ui/ShopEditor.java
+++ common/java/net/fexcraft/mod/fsmmshop/ui/ShopEditor.java
@@ -1,10 +1,18 @@
 package net.fexcraft.mod.fsmmshop.ui;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fsmm.util.Config;
+import net.fexcraft.mod.uni.EnvInfo;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.ui.UIButton;
 import net.fexcraft.mod.uni.ui.UserInterface;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -19,9 +27,52 @@ public class ShopEditor extends UserInterface {
 	}
 
 	@Override
+	public void init(){
+		fields.get("price").text(Config.getWorthAsString(con.shop.price, false));
+	}
+
+	@Override
+	public boolean onAction(UIButton button, String id, int x, int y, int mb){
+		switch(id){
+			case "admin":{
+				TagCW com = TagCW.create();
+				com.set("task", "admin");
+				container.SEND_TO_SERVER.accept(com);
+				return true;
+			}
+			case "confirm":{
+				TagCW com = TagCW.create();
+				com.set("task", "price");
+				com.set("price", format());
+				container.SEND_TO_SERVER.accept(com);
+				return true;
+			}
+			case "sell":{
+				TagCW com = TagCW.create();
+				com.set("task", "sell");
+				container.SEND_TO_SERVER.accept(com);
+				return true;
+			}
+			case "buy":{
+				TagCW com = TagCW.create();
+				com.set("task", "buy");
+				container.SEND_TO_SERVER.accept(com);
+				return true;
+			}
+			case "item":{
+				TagCW com = TagCW.create();
+				com.set("task", "item");
+				container.SEND_TO_SERVER.accept(com);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
 	public void predraw(float pticks, int mouseX, int mouseY) {
-		buttons.get("sell").tx = con.shop.sell ? 31 : 101;
-		buttons.get("buy").tx = con.shop.sell ? 101 : 31;
+		buttons.get("sell").tx = buttons.get("sell").htx = con.shop.sell ? 31 : 101;
+		buttons.get("buy").tx = buttons.get("buy").htx = con.shop.sell ? 101 : 31;
 		if(con.shop.admin){
 			texts.get("title").transval("ui.fsmmshop.shop_name.admin");
 		}
@@ -50,6 +101,23 @@ public class ShopEditor extends UserInterface {
 			list.add(con.shop.stack.getName());
 			//todo stack info
 			list.add(con.shop.stack.getID() + " | " + con.shop.stack.damage());
+		}
+	}
+
+	private static final DecimalFormat df = new DecimalFormat("#.000", new DecimalFormatSymbols(Locale.US));
+	static { df.setRoundingMode(RoundingMode.DOWN); }
+
+	private final long format(){
+		try{
+			String str = fields.get("price").text().replace(Config.DOT, "").replace(",", ".");
+			if(str.length() == 0) return 0;
+			String format = df.format(Double.parseDouble(str));
+			return Long.parseLong(format.replace(",", "").replace(".", ""));
+		}
+		catch(Exception e){
+			con.player.entity.send("INVALID INPUT: " + e.getMessage());
+			if(EnvInfo.DEV) e.printStackTrace();
+			return 0;
 		}
 	}
 
